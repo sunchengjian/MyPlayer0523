16fb16a30560a2f8317417ca6cfd12e46d7cbf31
sunchengjian <3230706893@qq.com>
2017-05-23 22:12:59 +0800

    实现底部按钮的点击监听事件，建立一个基类，有公共的特点，使使四个pager继承他

---
+++ app/src/main/java/com/sunchengjian/myplayer0523/pager/NetAudio.java
@@ -0,0 +1,30 @@
+package com.sunchengjian.myplayer0523.pager;
+
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.sunchengjian.myplayer0523.frament.BaseFragment;
+
+/**
+ * Created by 0..0 on 2017/5/23.
+ */
+
+public class NetAudio extends BaseFragment {
+    private TextView textView;
+    @Override
+    public View initView() {
+        textView = new TextView(context);
+        textView.setTextSize(30);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextColor(Color.RED);
+        return textView;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        textView.setText("444");
+    }
+}
